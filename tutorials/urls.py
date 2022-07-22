b40402454f262d71518f3874debf83bf65d55f25
repoleved08 <geekbii08@gmail.com>
2022-07-22 09:15:17 +0300

    First Commit (djrestAPI)

---
+++ tutorials/urls.py
@@ -0,0 +1,17 @@
+
+from django.contrib import admin
+from django.urls import include, path
+from django.contrib.auth.models import User
+from rest_framework import routers
+from quickstart import serializers, views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+]
